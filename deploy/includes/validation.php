77fad737d427147f328625635cbdea31b90a352a
aymanbb <aymancool30@gmail.com>
2026-02-17 15:37:11 +0100

    Small fixes
    Will elaborate more on merge

---
+++ deploy/includes/validation.php
@@ -0,0 +1,152 @@
+<?php
+declare(strict_types=1);
+
+// TODO: Maybe best to use a return type?
+// enum validationResult{
+//     case valid;
+//     case invalid;
+//     case do_not_trust;
+// }
+
+
+/// DOC: Functions for validating site data.
+
+function validateUsername($username):bool{
+    $pattern = "/^[a-zA-Z ]{2,50}$/";
+
+    if (preg_match($pattern, $username) === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail($email):bool{
+    $result = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+    if ($result === false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validatePassword($password):bool{
+    $pattern = "/^[a-zA-Z0-9 !@#$%^&*()_+={}:|,.?~`]{16,64}$/";
+
+    if (preg_match($pattern, $password) === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateSubjectName($subject):bool{
+    $pattern = "/^[a-zA-Z]{2,50}$/";
+
+    if (preg_match($pattern, $subject)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateSubjectCode(string $subject_code):bool{
+    $pattern = "/^[a-zA-Z]{3}[0-9]{5}$/";
+
+    if (preg_match($pattern, $subject_code)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateSubjectPin($pin):bool{
+    $pattern = "/^[0-9]{4}$/";
+
+    if (preg_match($pattern, $pin)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validateMessageID($id):bool{
+    $pattern = "/[0-9+]/";
+
+    if (preg_match($pattern, $id)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateFreetext($text):bool{
+    $characterCap = 500;
+
+    if (!empty(trim($text)) && (strlen($text) <= $characterCap)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Verifies that the uploaded file meets size requirements, matches allowed MIME types and is uploaded without error.
+ * @param array $file Associative array from $_FILES['input_name'].
+ * @return string The temporary filepath, if validation passes.
+ * @throws RuntimeException If the PHP upload error code is not UPLOAD_ERR_OK.
+ * @throws UnexpectedValueException If the MIME type is not permitted.
+ * @throws LengthException If the file exceeds $maxSize.
+ */ 
+ function validateImage(array $file):string{
+
+    if ($file['error'] !== UPLOAD_ERR_OK){
+
+        throw new RuntimeException("Upload failed with error code: " . $file['error']);
+    }
+
+    $maxSize = 1024*1024; // = 1MB
+    $allowedTypes = ['image/jpeg', 'image/png'];
+
+    //PHPs own methods read the "magic information" off the file directly.
+    $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+    $fileType = finfo_file($fileInfo, $file['tmp_name']);
+    finfo_close($fileInfo);
+
+    if (!in_array($fileType, $allowedTypes)) {
+        //does not display the incorrect filetype to avoid educating attackers.
+        throw new UnexpectedValueException("Invalid file type.");
+    }
+    if ($file['size'] > $maxSize) {
+        throw new LengthException("File exceeds maximum size.");
+    }
+    return $file['tmp_name'];
+} 
+
+/**
+ * Generates a unique filename by making a content hash, moves the file to designated storage.
+ * @param string $temporaryLocation Path to the temporary file, returned by validateImage().
+ * @param string $fileDestinationFolder Target directory for storage.
+ * @throws RuntimeException If the file cannot be moved to target destination.
+ */
+function moveUploadedFile(string $temporaryLocation, string $fileDestinationFolder):void{
+
+    $fileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $temporaryLocation);
+    $extensions = [
+        "image/jpeg" => "jpg",
+        "image/png" => "png"
+    ];
+    //TODO: decide: Will this allow .exe, for instance ? should it also be disallowed here?
+    $extension = $extensions[$fileType] ?? "bin";
+
+     //prevets duplicates and sanitizes - hash based on the file's binary data
+    $uniqueName = md5_file($temporaryLocation);
+    $fullpath = rtrim($fileDestinationFolder, "/") . "/" . $uniqueName . "." . $extension;  
+
+    if (file_exists($fullpath)) return;
+    if (!move_uploaded_file($temporaryLocation, $fullpath)){
+            throw new RuntimeException("Failed to move $temporaryLocation to $fileDestinationFolder.");
+    }
+
+} 
+
---
+++ deploy/subject_messages.php
@@ -0,0 +1,181 @@
+<?php
+
+require_once __DIR__ . '/includes/config.php';
+require_once __DIR__ . '/includes/database.php';
+require_once __DIR__ . '/includes/session.php';
+
+$db = new Database();
+
+// NOTE: det skal være mulig å hente "subject pin" fra $_GET['ref'] her, om man blir omdirigert fra guest_login.php 
+// Burde det være en default verdi??
+$subject_pin = "6666";
+if(validateSubjectPin($_GET['ref'])){
+    $subject_pin = $_GET['ref'];
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    if(isset($_GET['test-melding-submit'])){
+        $user_id = SessionGetUserID();
+        $new_message = htmlspecialchars($_GET["test-melding"]);
+        //answer
+        //subject_ID
+        $db->subjectMessageSubmit($user_id, (int)$subject_pin, $new_message);
+    } 
+    $subject_messages = $db->subjectMessageFetchAll($subject_pin);  
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= htmlspecialchars($emnenavn) ?>-meldinger</title>
+        <style>
+            body {
+
+                #skip {
+                    bottom: 0;
+                    position: fixed;
+                    right: 3rem;
+                    margin: 1rem;
+                    padding: 1rem;
+                    height: 3rem;
+                    width: fit-content;
+                    align-items: center;
+                    display: flex;
+                    font-size: 16px;
+                    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+                    text-decoration: none;
+                    border: 3px solid black;
+                }
+
+                nav{
+                    margin: 0.5rem;
+
+                    a{
+                        text-decoration: none;
+                    }
+                } 
+
+                button {
+                    max-width: max-content;
+                }
+
+                section {
+                    display: flex;
+                    flex-direction: column;
+
+                    h1 {
+                        justify-content: center;
+                        display: flex;
+                    }
+
+                    nav ul {
+                        padding: 0;
+                        list-style: none;
+                        margin: auto;
+                        display: flex;
+                        justify-content: center;
+
+                        li {
+                            margin: 0.5rem;
+
+                            a{
+                                text-decoration: none;
+                            }
+                        }
+                    }
+
+                    article {
+                        border: 3px solid black;
+                        padding: 2rem;
+                        width: 50dvw;
+                        margin: 1rem auto 1rem auto;
+                        
+
+                        .melding{
+                            border: 1px solid black;
+                            padding: 4px;
+                        }
+                    }
+                }
+            
+                article {
+                    display: flex;
+                    flex-direction: column;
+                    border: 3px solid black;
+                    padding: 2rem;
+                    width: 50dvw;
+                    margin: 1rem auto 1rem auto;
+
+                    h2{
+                        width: 50dvw;
+                    }
+
+                    img{
+                        max-width: 150px;
+                    }
+
+                    form {
+                        display: flex;
+                        flex-direction: column;
+                        width: 50dvw;
+                        margin: 1rem auto 1rem auto;
+
+                        textarea {
+                            resize: none;
+                        }
+
+                        button {
+                            padding: 3px 10px 3px 10px;
+                            margin-top: 0.5rem;
+                        }
+                    }
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <a href="#send_message" id="skip">Hopp til bunnen</a>
+        <section>
+            <h1><?= htmlspecialchars($emnenavn) ?></h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Gå til forsiden</a></li>
+                    <li><a href="guest_login.php">Fortsett som gjest</a></li>
+                    <li><a href="#">Glemt passord?</a></li>
+                    <li><a href="subject_messages.php">Meldinger - HUSK Å FJERNE</a></li>
+                    <li><a href="emneoversikt.php">Emneoversikt ditto</a></li>
+                </ul>
+            </nav>
+            <article>
+                <h2>Foreleser</h2>
+                <p>Foreleser for <?= htmlspecialchars($emnenavn) ?> er <?= htmlspecialchars($forelesernavn) ?>. 
+                Kan nås på e-post: <a href="mailto:<?= htmlspecialchars($foreleserepost) ?>"><?= htmlspecialchars($foreleserepost) ?></a></p>
+            <!-- Filsti korrekt ? fungerer ? funnet på variabelnavn... -->
+                <img src="/media/<?= htmlspecialchars($myvar) ?>" alt="Bilde av foreleser">
+            </article>
+            <?php foreach ($subject_messages as $subject_message): ?>
+                <article>
+                    <h3><?= 'Message_ID: ' . htmlspecialchars($subject_message['Message_ID']) . " " ?>Fra anonym:</h3>
+                    <p class="message"><?= htmlspecialchars($subject_message['Message_body']) ?></p>
+                </article>
+            <?php endforeach; ?>
+        </section>
+        <article>
+            <h2>Delta i samtalen!</h2>
+            <?php if ($message != ""): ?>
+                <p class="melding"><?= htmlspecialchars($message) ?></p>
+            <?php endif; ?>
+            
+            <form method="get">
+                <label for="test-melding" id="send_message">Skriv din melding her</label>
+                <textarea name="test-melding" maxlength="256" rows="10" cols="50" required></textarea>            
+                <button type="submit" name="test-melding-submit">Send</button>  
+                <input type="hidden" name="ref" value="<?php echo $subject_pin; ?>">              
+            </form>
+        </article>
+    </body>
+</html>
