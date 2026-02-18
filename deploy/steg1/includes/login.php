e2e372f4612bff3b1183e09cdc41cc15c3bf5892
TFjereide <torgeirfjereide@gmail.com>
2026-02-18 16:58:16 +0100

    maybe???

--- deploy/steg1/includes/login.php
+++ deploy/steg1/includes/login.php
@@ -31,5 +31,33 @@ class Login {
             }
         }
     }
+     public function api_login(string $user, string $pass) {
+        $db = new Database();
+        $username = $user;
+        $password = $pass;
+
+        if (empty($username) || empty($password)) {
+            $message = "All fields are required.";
+        } elseif (!isLockedOut()) {
+            $user = $db->userFindByUsername($username);
+            if ($user && password_verify($password, $user['password'])) {
+                session_regenerate_id(true);
+                $_SESSION['user'] = [
+                    'id' => $user['id'],
+                    'username' => $username
+                ];
+                $_SESSION['logged_in'] = true;
+                return true;
+            } else {
+                $_SESSION['login_attempts']++;
+                $_SESSION['login_error'] = "Invalid username or password.";
+                if (isLockedOut()) {
+                    echo "You're locked out! Please wait a few minutes before trying again.";
+                }
+            }
+        }
+        
+        return false;
+    }
 }
 ?>
